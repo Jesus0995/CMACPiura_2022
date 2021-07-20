c7f88ca57f00e73da0bf0e7793cbf3fc5a94f439
magaly churata <misteriosagothic@gmail.com>
2021-07-20 15:14:08 -0500

    Cambio de driver firefox

--- src/test/java/Definitions/Hooks.java
+++ src/test/java/Definitions/Hooks.java
@@ -4,7 +4,7 @@ package Definitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class Hooks {
@@ -13,12 +13,14 @@ public class Hooks {
 
     @Before
     public static void setUp(){
-        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/geckodriver.exe");
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
     }
 
     @After
+
+
 
     public static void tearDown(){
         driver.manage().deleteAllCookies();
