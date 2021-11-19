65f98ecc068d5f4ea1f8d310b6ba11ea6b532c5e
Magaly Churata <misteriosagothic@gmail.com>
2021-11-19 09:52:20 -0500

    Cambio de nombre de page Defina Propuesta

--- src/test/java/PageObjects/DefinaPropuestaPage.java
+++ src/test/java/PageObjects/DefinaPropuestaPage.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class DefinalaPropuestaPage {
+public class DefinaPropuestaPage {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    public DefinalaPropuestaPage(WebDriver d) {
+    public DefinaPropuestaPage(WebDriver d) {
         driver = d;
         wait = new WebDriverWait(driver, 50);
         PageFactory.initElements(driver, this);
